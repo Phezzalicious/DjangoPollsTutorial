ca8e25d470adbbe5a943ec9128319122a4496995
phelps <starfish206@gmail.com>
2019-12-19 05:51:59 -0600

    BSOD sucks, Generic Views part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,23 +4,27 @@ from .models import Choice, Question
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+  
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
